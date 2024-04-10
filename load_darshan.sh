e22141996907eecd9f5912fdb4b15763ecd63650
Zhaobin Zhu <zhu.zhaobin@hotmail.de>
2024-04-10 13:57:48 +0200

    add copy

--- load_darshan.sh
+++ load_darshan.sh
@@ -6,7 +6,7 @@ export DARSHAN_LOG_DIR_PATH=.
 
 # Define the parameters
 dims="10 100 1000 10000"
-input_path="/hdd-share/generated-matrix/"
+input_path="$PWD"
 output_path="/hdd-share/generated-matrix/"
 
 
@@ -14,7 +14,7 @@ export DARSHAN_ENABLE_NONMPI=1
 # Loop through each combination of parameters
 for d in $dims; do
   echo "Running program with --rows=$d --cols=$d"
-  LD_PRELOAD=/home/luser/Repos/spack/opt/spack/linux-almalinux9-ivybridge/gcc-11.4.1/darshan-runtime-3.4.4-ykayvipyjq7hnrzq7qeqs5ekpn6nga4t/lib/libdarshan.so /cmatrix_generator -i $input_path -o $output_path -cols $d -r $d
+  LD_PRELOAD=/home/luser/Repos/spack/opt/spack/linux-almalinux9-ivybridge/gcc-11.4.1/darshan-runtime-3.4.4-ykayvipyjq7hnrzq7qeqs5ekpn6nga4t/lib/libdarshan.so /cmatrix_generator -i $input_path -o $output_path -cols $d -r $d --copy
   # ssh to remote machine and execute the consumer program
   # ssh -t user@remote_machine "LD_PRELOAD=/path_to_remote/lib/libdarshan.so /matrix_operations -i $output_path -o $output_path"
   echo "Done"
--- main.cpp
+++ main.cpp
@@ -9,13 +9,15 @@
 
 
 #define BENCHMARK
+static int copy_flag = false;
 
-template <typename T>
+
+template<typename T>
 std::vector<std::vector<T> > generate_dense_matrix(int rows, int cols, int num_non_zero, int max_value) {
     std::vector<std::vector<T> > matrix(rows, std::vector<T>(cols, 0));
     std::random_device rd;
     std::mt19937 gen(rd());
-    for(int i = 0; i < num_non_zero; ++i) {
+    for (int i = 0; i < num_non_zero; ++i) {
         std::uniform_int_distribution<int> row_dist(0, rows - 1);
         std::uniform_int_distribution<int> col_dist(0, cols - 1);
         int row = row_dist(gen);
@@ -31,8 +33,8 @@ std::vector<std::vector<T> > generate_dense_matrix(int rows, int cols, int num_n
     return matrix;
 }
 
-template <typename T>
-void write_matrix_market_format(const std::vector<std::vector<T> >& matrix, const std::string& file_name) {
+template<typename T>
+void write_matrix_market_format(const std::vector<std::vector<T> > &matrix, const std::string &file_name) {
     std::ofstream file(file_name);
     file << "%%MatrixMarket matrix coordinate real general" << std::endl;
     file << matrix.size() << " " << matrix[0].size() << " " << matrix.size() * matrix[0].size() << std::endl;
@@ -44,9 +46,15 @@ void write_matrix_market_format(const std::vector<std::vector<T> >& matrix, cons
     file.close();
 }
 
+void copy_file(const std::string &src, const std::string &dst) {
+    std::ifstream src_file(src, std::ios::binary);
+    std::ofstream dst_file(dst, std::ios::binary);
+    dst_file << src_file.rdbuf();
+}
 
-std::ifstream::pos_type get_filesize(const char* filename) {
-    std::ifstream in(filename, std::ifstream ::ate | std::ifstream::binary);
+
+std::ifstream::pos_type get_filesize(const char *filename) {
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
     return in.tellg();
 }
 
@@ -58,24 +66,37 @@ std::string get_file_name_from_rows_and_cols(int rows, int cols, bool is_dense) 
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     std::filesystem::path write_file_path{};
     std::filesystem::path read_file_path{};
     int cols, rows;
-    const char* const short_opts = "i:o:c:r:";
+    const char *const short_opts = "i:o:c:r:";
     static const struct option long_opts[] = {
-            {"input", required_argument, nullptr, 'i'},
-            {"output", required_argument, nullptr, 'o'},
-            {"cols", required_argument, nullptr, 'c'},
-            {"rows", required_argument, nullptr, 'r'},
+            {"input",   required_argument, nullptr, 'i'},
+            {"output",  required_argument, nullptr, 'o'},
+            {"cols",    required_argument, nullptr, 'c'},
+            {"rows",    required_argument, nullptr, 'r'},
+            {"copy", no_argument, &copy_flag, true},
+
 
     };
     while (true) {
+        int option_index = 0;
         const auto opt =
                 getopt_long(argc, argv, short_opts, long_opts, nullptr);
         if (opt == -1)
             break;
         switch (opt) {
+            case 0:
+                if (long_opts[option_index].flag != nullptr)
+                    break;
+                printf ("option %s", long_opts[option_index].name);
+                if (optarg)
+                    printf (" with arg %s", optarg);
+                printf ("\n");
+                break;
+
+                break;
             case 'i':
                 read_file_path = optarg;
                 break;
@@ -106,23 +127,46 @@ int main(int argc, char* argv[]) {
         std::cerr << "invalid matrix dimensions" << std::endl;
         std::terminate();
     }
-    std::vector<std::vector<double>> matrix = generate_dense_matrix<double>(cols, rows, cols*rows, 1);
-#if defined(BENCHMARK)
-    auto t_begin = std::chrono::high_resolution_clock::now();
-#endif
+    if (copy_flag) {
+        std::cout << "copy flag is set" << std::endl;
+    }
+    std::vector<std::vector<double>> matrix = generate_dense_matrix<double>(cols, rows, cols * rows, 1);
     std::string filename = get_file_name_from_rows_and_cols(rows, cols, true);
     write_file_path += filename;
-    write_matrix_market_format(matrix, write_file_path);
+    read_file_path += filename;
+    if (!copy_flag) {
 #if defined(BENCHMARK)
-    auto t_end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(t_end - t_begin);
-    std::cout <<  get_filesize((char*)write_file_path.c_str())<< std::endl;
-    std::cout << "Elapsed time for generated code " << duration.count() << " seconds" << std::endl;
-    auto f_size = get_filesize((char*)write_file_path.c_str());
-    std::cout << "Matrix dimensions: " << rows << "x" << cols << std::endl;
-    std::cout << "File size (Byte): " << f_size << std::endl;
-    std::cout << "Bandwidth (Byte/s): " << f_size / duration.count() << std::endl;
+        auto t_begin = std::chrono::high_resolution_clock::now();
 #endif
+        write_matrix_market_format(matrix, write_file_path);
+#if defined(BENCHMARK)
+        auto t_end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(t_end - t_begin);
+        std::cout << get_filesize((char *) write_file_path.c_str()) << std::endl;
+        std::cout << "Elapsed time for write the matrix " << duration.count() << " seconds" << std::endl;
+        auto f_size = get_filesize((char *) write_file_path.c_str());
+        std::cout << "Matrix dimensions: " << rows << "x" << cols << std::endl;
+        std::cout << "File size (Byte): " << f_size << std::endl;
+        std::cout << "Bandwidth (Byte/s): " << f_size / duration.count() << std::endl;
+#endif
+    } else {
+        std::cout << "copying the file" << std::endl;
+        write_matrix_market_format(matrix, read_file_path);
+#if defined(BENCHMARK)
+        auto t_begin = std::chrono::high_resolution_clock::now();
+#endif
+        copy_file(read_file_path, write_file_path);
+#if defined(BENCHMARK)
+        auto t_end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(t_end - t_begin);
+        std::cout << get_filesize((char *) write_file_path.c_str()) << std::endl;
+        std::cout << "Elapsed time for copy the matrix file " << duration.count() << " seconds" << std::endl;
+        auto f_size = get_filesize((char *) write_file_path.c_str());
+        std::cout << "Matrix dimensions: " << rows << "x" << cols << std::endl;
+        std::cout << "File size (Byte): " << f_size << std::endl;
+        std::cout << "Bandwidth (Byte/s): " << f_size / duration.count() << std::endl;
+#endif
+    }
     return 0;
 }
 
