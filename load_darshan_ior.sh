d2eb65faf78fba7896ae4714daa075579a455c05
Zhaobin Zhu <zhu.zhaobin@hotmail.de>
2024-04-15 11:53:53 +0200

    change ior command

--- load_darshan_ior.sh
+++ load_darshan_ior.sh
@@ -2,7 +2,7 @@
 
 # Define the parameters
 tsize="4k 1m 2m"
-bsize="200k 20m 2g"
+bsize="200k 20m 1g 2g"
 output_path="/hdd-share/ior_out/test.out"
 
 
@@ -14,8 +14,8 @@ for t in $tsize; do
     for b in $bsize; do
             echo "Running program with -t = $t -b = $b"
             # for HPCA004
-            LD_PRELOAD=/home/luser/Repos/spack/opt/spack/linux-almalinux9-ivybridge/gcc-11.4.1/darshan-runtime-3.4.4-ykayvipyjq7hnrzq7qeqs5ekpn6nga4t/lib/libdarshan.so ior -a POSIX -b "$b" -t "$t" -e -C -o "$output_path"
+            LD_PRELOAD=/home/luser/Repos/spack/opt/spack/linux-almalinux9-ivybridge/gcc-11.4.1/darshan-runtime-3.4.4-ykayvipyjq7hnrzq7qeqs5ekpn6nga4t/lib/libdarshan.so ior -a POSIX -b "$b" -t "$t" -e -C -o "$output_path" --posix.odirect
             # for HPCA003
-            # LD_PRELOAD=/home/luser/Repos/spack/opt/spack/linux-almalinux9-ivybridge/gcc-11.4.1/darshan-runtime-3.4.4-3elp6ce775ewdazycqrxmkbyi6em2x76/lib/libdarshan.so ior -a POSIX -b "$b" -t "$t" -e -C -o "$output_path"
+            # LD_PRELOAD=/home/luser/Repos/spack/opt/spack/linux-almalinux9-ivybridge/gcc-11.4.1/darshan-runtime-3.4.4-3elp6ce775ewdazycqrxmkbyi6em2x76/lib/libdarshan.so ior -a POSIX -b "$b" -t "$t" -e -C -o "$output_path" --posix.odirect
     done
 done
